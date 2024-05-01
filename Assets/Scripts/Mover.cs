d320fbdd456680b2a8272704062b1a38098feeff
Christopher Yang <christopher.yang01@gmail.com>
2024-04-30 19:22:42 -0500

    Sparated Player Input and movement logic

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,7 +4,6 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private Ray lastRay;
 
     private void Start()
     {
@@ -13,23 +12,12 @@ public class Mover : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(1))
-        {
-            MoveToMouse();
-        }
         UpdateAnimator();
     }
 
-    private void MoveToMouse()
+    public void MoveTo(Vector3 destination)
     {
-        RaycastHit hit;
-        lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        bool hasHit = Physics.Raycast(lastRay, out hit);
-        if (hasHit)
-        {
-            agent.SetDestination(hit.point);
-            Debug.DrawRay(lastRay.origin, lastRay.direction * 25f);
-        }
+        agent.SetDestination(destination);
     }
 
     private void UpdateAnimator()
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            MoveToMouse();
+        }
+    }
+    private void MoveToMouse()
+    {
+        RaycastHit hit;
+        Ray lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool hasHit = Physics.Raycast(lastRay, out hit);
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
