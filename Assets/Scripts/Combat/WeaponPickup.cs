fee6fcb2b020c34d104991e71bada7d51c7ed37b
Christopher Yang <christopher.yang01@gmail.com>
2024-08-20 17:21:48 -0500

    Made WeaponPickup Respawn

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TheGuild.Control;
 using UnityEngine;
 
@@ -6,13 +7,31 @@ namespace TheGuild.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private WeaponSO weaponSO;
+        [SerializeField] private float respawnTime = 5f;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent(out PlayerController playerController))
             {
                 playerController.GetComponent<Fighter>().EquipWeapon(weaponSO);
-                Destroy(gameObject);
+                StartCoroutine(PickupRoutine());
+            }
+        }
+
+        private IEnumerator PickupRoutine()
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(respawnTime);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
